01b8951eff14a8d29ec57379e334e9892fceaf30
Carlos Vera <c.v004@hotmail.com>
2018-09-13 00:55:23 -0400

    Completed Part1 of Hw1

--- threads/main.cc
+++ threads/main.cc
@@ -59,7 +59,7 @@ extern int testnum;
 
 // External functions used by this file
 
-extern void ThreadTest(void), Copy(char *unixFile, char *nachosFile);
+extern void ThreadTest(int), Copy(char *unixFile, char *nachosFile);
 extern void Print(char *file), PerformanceTest(void);
 extern void StartProcess(char *file), ConsoleTest(char *in, char *out);
 extern void MailTest(int networkID);
@@ -101,7 +101,7 @@ main(int argc, char **argv)
       }
     }
 
-    ThreadTest();
+    ThreadTest(5);
 #endif
 
     for (argc--, argv++; argc > 0; argc -= argCount, argv += argCount) {
--- threads/threadtest.cc
+++ threads/threadtest.cc
@@ -11,6 +11,7 @@
 
 #include "copyright.h"
 #include "system.h"
+#include "synch.h"
 
 // testnum is set in main.cc
 int testnum = 1;
@@ -24,31 +25,67 @@ int testnum = 1;
 //	purposes.
 //----------------------------------------------------------------------
 
-void
-SimpleThread(int which)
+#define HW1_SEMAPHORES
+
+int SharedVariable;
+
+#ifdef HW1_SEMAPHORES
+Semaphore* semaphore;
+Semaphore* barrier;
+int BarrierCounter;
+#endif
+
+
+
+void SimpleThread(int which)
 {
-    int num;
+    int num, val;
     
-    for (num = 0; num < 5; num++) {
-	printf("*** thread %d looped %d times\n", which, num);
+    for( num = 0; num < 5; num++ ) {
+#ifdef HW1_SEMAPHORES
+        semaphore->P(); //request the main semaphore
+#endif
+        val = SharedVariable;
+        printf("*** thread %d sees value %d\n", which, val);
+        currentThread->Yield();
+        SharedVariable = val+1;
+        
+#ifdef HW1_SEMAPHORES
+        semaphore->V(); //release the main semaphore
+#endif
         currentThread->Yield();
     }
+
+#ifdef HW1_SEMAPHORES
+    barrier->P(); //request the barrier semaphore
+    BarrierCounter--;   //indicate that one of the threads has finished processing
+                        //and is now waiting on any others
+    barrier->V(); //release the barrier semaphore
+    while (BarrierCounter > 0) {
+        currentThread->Yield();
+    }
+#endif    
+    
+    val = SharedVariable;
+    printf("Thread %d sees final value %d\n", which, val);
 }
 
 //----------------------------------------------------------------------
 // ThreadTest1
-// 	Set up a ping-pong between two threads, by forking a thread 
+// 	Set up a ping-pong between n threads, by forking a thread 
 //	to call SimpleThread, and then calling SimpleThread ourselves.
 //----------------------------------------------------------------------
 
 void
-ThreadTest1()
-{
+ThreadTest1(int n)
+{    
     DEBUG('t', "Entering ThreadTest1");
 
-    Thread *t = new Thread("forked thread");
-
-    t->Fork(SimpleThread, 1);
+    Thread *t[n];
+    while (--n > 0) {
+        t[n] = new Thread("forked thread");
+        t[n]->Fork(SimpleThread, n);
+    }
     SimpleThread(0);
 }
 
@@ -58,15 +95,27 @@ ThreadTest1()
 //----------------------------------------------------------------------
 
 void
-ThreadTest()
+ThreadTest(int n)
 {
-    switch (testnum) {
-    case 1:
-	ThreadTest1();
-	break;
-    default:
-	printf("No test specified.\n");
-	break;
+    if (n == 0) {
+        return;
+    } else if (n < 0) {
+        printf("No test specified.\n");
     }
+    
+#ifdef HW1_SEMAPHORES //instantiate and allocate semaphore variables and barrier counter
+    BarrierCounter = n;
+    semaphore = new Semaphore("s", 1);
+    barrier = new Semaphore("b", 1);
+#endif
+    
+    ThreadTest1(n);
+    
+#ifdef HW1_SEMAPHORES //deallocate semaphore variables
+    delete semaphore;
+    semaphore = 0;
+    delete barrier;
+    barrier = 0;
+#endif
 }
 
